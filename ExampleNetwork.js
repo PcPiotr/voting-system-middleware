78d25e3e48e76fba129cf687fa07ee0f3d569dfc
PcPiotr <34127309+PcPiotr@users.noreply.github.com>
2019-05-26 18:48:08 +0200

    fixes

---
+++ ExampleNetwork.js
@@ -0,0 +1,37 @@
+var fabricClient = require('./Config/FabricClient');
+class ExampleNetwork {
+  constructor(userName) {
+    this.currentUser;
+    this.issuer;
+    this.userName = userName;
+    this.connection = fabricClient;
+  }
+  init() {
+    var isAdmin = false;
+    if (this.userName == "admin") {
+      isAdmin = true;
+    }
+    return this.connection.initCredentialStores().then(() => {
+      return this.connection.getUserContext(this.userName, true)
+    }).then((user) => {
+      this.issuer = user;
+      if (isAdmin) {
+        return user;
+      }
+      return this.ping();
+    }).then((user) => {
+      this.currentUser = user;
+      return user;
+    })
+  }
+   sell(data) {
+    var tx_id = this.connection.newTransactionID();
+    var requestData = {
+      fcn: 'init',
+      args: [data],
+      txId: tx_id
+    };
+    var request = FabricModel.requestBuild(requestData);
+    return this.connection.submitTransaction(request);
+  }
+}
